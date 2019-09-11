c06538ad951b3e1252cac5edf989d266be017c34
Wanying Xie <18811503900@sina.cn>
2019-09-11 15:22:25 +0800

    Delete classifier8.sh

--- classifier8.sh
+++
@@ -1,3 +0,0 @@
-export BERT_BASE_DIR=/home/wanying/bert-mce/chinese_L-12_H-768_A-12
-export MY_DATASET=/home/wanying/bert-mce/data
-python3 run_classifier.py --use_tpu=False --task_name=MCE --do_train=true --do_eval=true --do_predict=true --data_dir=$MY_DATASET --vocab_file=$BERT_BASE_DIR/vocab.txt  --bert_config_file=$BERT_BASE_DIR/bert_config.json --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt --max_seq_length=128 --train_batch_size=16 --learning_rate=3e-5 --num_train_epochs=2.0 --output_dir=./MCE_8/
